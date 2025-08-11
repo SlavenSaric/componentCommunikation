3185c677e332abea956c04d34f8ae2cf46d3a0d6
SlavenSaric <slavendownload@gmail.com>
2025-08-11 18:59:39 +0200

    first component

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import { createApp } from 'vue';
+import App from './App.vue'
+import FriendContact from './components/FriendContact.vue'
 
-createApp({}).mount('');
+const app = createApp(App)
+app.mount('#app');
+
+app.component('friend-contact', FriendContact)
