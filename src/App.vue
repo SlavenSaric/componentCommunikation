cf41e1927ef17c49c74b73009a16d3bd23b73b14
SlavenSaric <slavendownload@gmail.com>
2025-08-16 07:35:36 +0200

    form added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <header><h1>My Friends</h1></header>
+  <new-friend @add-contact="addContact"></new-friend>
   <ul>
     <friend-contact
       v-for="friend in friends"
@@ -16,8 +17,9 @@
 
 <script>
 import FriendContact from "./components/FriendContact.vue";
+import NewFriend from "./components/NewFriend.vue";
 export default {
-  components: { FriendContact },
+  components: { FriendContact, NewFriend },
   data() {
     return {
       friends: [
@@ -42,6 +44,17 @@ export default {
     toggleFavoriteStatus(friendId){
       const indentifiedFriend = this.friends.find(friend => friend.id === friendId)
       indentifiedFriend.isFavorite = !indentifiedFriend.isFavorite
+    },
+    addContact(name, phone, email){
+      const newFriend = {
+        id: new Date().toDateString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false
+      }
+
+      this.friends.push(newFriend)
     }
   }
 };
@@ -79,7 +92,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -111,5 +125,19 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
